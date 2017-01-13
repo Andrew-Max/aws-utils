c3d80e2d0d6396fbe817f6a0ba34bf30d7c16e9e
andrew <anm898989@gmail.com>
2017-01-13 02:10:18 -0500

    recursively uploads individual parts

--- node_glacier_multi_part.js
+++ node_glacier_multi_part.js
@@ -3,86 +3,85 @@ var creds = require('/home/yotta/.aws')
 var fs =  require('fs');
 var filePath = '/home/yotta/zips/pics/100.zip';
 var encoding = "utf8";
-
+var multipart;
 var myConfig = new AWS.Config({
   accessKeyId: creds.AccessKeyID,
   secretAccessKey: creds.SecretAccessKey,
   region: 'us-west-1'
 });
-
 var glacier = new AWS.Glacier(myConfig)
-
 var buffer = fs.readFileSync(filePath);
-// var buffer = new Buffer(2.5 * 1024 * 1024); // 2.5MB buffer
+// var buffer = new Buffer(25 * 1024 * 1024); // 2.5MB buffer
 var partSize = 1024 * 1024; // 1MB chunks,
 var numPartsLeft = Math.ceil(buffer.length / partSize);
 var startTime = new Date();
+var byteIncrementer = 0;
 
 var params = {
   accountId: '-',
-  vaultName: 'amax-photos',
+  // vaultName: 'amax-photos',
+  vaultName: 'amax-test',
   archiveDescription: '100media',
+  archiveDescription: 'test',
   partSize: partSize.toString(),
 };
 
-// Compute the complete SHA-256 tree hash so we can pass it
-// to completeMultipartUpload request at the end
 var treeHash = glacier.computeChecksums(buffer).treeHash;
 
-// Initiate the multipart upload
-console.log('Initiating upload to', params.vaultName);
-glacier.initiateMultipartUpload(params, function (mpErr, multipart) {
-    if (mpErr) { console.log('Error!', mpErr.stack); return; }
-    console.log("Got upload ID", multipart.uploadId);
+new Promise(function (resolve, reject) {
+    glacier.initiateMultipartUpload(params, function (mpErr, multi) {
+        if (mpErr) { console.log('Error!', mpErr.stack); return; }
+        console.log("Got upload ID", multi.uploadId);
+        multipart = multi
+        resolve();
+    });
+}).then(function () {
+    recursivelyUploadPart(byteIncrementer)
+}).catch(function (err) {console.log(err)});
 
-    // Grab each partSize chunk and upload it as a part
-    for (var i = 0; i < buffer.length; i += partSize) {
-        var end = Math.min(i + partSize, buffer.length),
-            partParams = {
-                accountId: '-',
-                vaultName: params.vaultName,
-                uploadId: multipart.uploadId,
-                range: 'bytes ' + i + '-' + (end-1) + '/*',
-                body: buffer.slice(i, end)
-            };
+function recursivelyUploadPart() {
+    var end = Math.min(byteIncrementer + partSize, buffer.length);
 
-        // Send a single part
-        console.log('body length: ', partParams.body.length)
-        console.log('Uploading part', i, '=', partParams.range);
+    var partParams = {
+        accountId: '-',
+        uploadId: multipart.uploadId,
+        vaultName: params.vaultName,
+        range: 'bytes ' + byteIncrementer + '-' + (end-1) + '/*',
+        body: buffer.slice(byteIncrementer, end)
+    };
 
-        glacier.uploadMultipartPart(partParams, function(multiErr, mData) {
-            if (multiErr) {
-                console.log('multiErr: ', multiErr)
-                return;
-            }
-
+    console.log('Uploading part', byteIncrementer, '=', partParams.range);
+    glacier.uploadMultipartPart(partParams, function(multiErr, mData) {
+        if (multiErr) {
+            console.log('part upload error: ', multiErr)
+            console.log('retrying')
+            return recursivelyUploadPart(byteIncrementer)
+        } else {
             console.log("Completed part", this.request.params.range);
 
             if (--numPartsLeft > 0) {
-                console.log('no parts left: finished')
-                return; // complete only when all parts uploaded
+                byteIncrementer += partSize;
+                console.log('recursing')
+                return recursivelyUploadPart(byteIncrementer)
+            } else {
+                var doneParams = {
+                    vaultName: params.vaultName,
+                    uploadId: multipart.uploadId,
+                    archiveSize: buffer.length.toString(),
+                    checksum: treeHash // the computed tree hash
+                };
+                console.log("Completing upload...");
+                resolve(glacier.completeMultipartUpload(doneParams, function(err, data) {
+                    if (err) {
+                        console.log("An error occurred while uploading the archive: ", err);
+                    } else {
+                        var delta = (new Date() - startTime) / 1000;
+                        console.log('Completed upload in', delta, 'seconds');
+                        console.log('Archive ID:', data.archiveId);
+                        console.log('Checksum:  ', data.checksum);
+                    }
+                }));
             }
-
-            var doneParams = {
-                vaultName: params.vaultName,
-                uploadId: multipart.uploadId,
-                archiveSize: buffer.length.toString(),
-                checksum: treeHash // the computed tree hash
-            };
-
-            console.log("Completing upload...");
-
-            glacier.completeMultipartUpload(doneParams, function(err, data) {
-                if (err) {
-                    console.log("An error occurred while uploading the archive");
-                    console.log(err);
-                } else {
-                    var delta = (new Date() - startTime) / 1000;
-                    console.log('Completed upload in', delta, 'seconds');
-                    console.log('Archive ID:', data.archiveId);
-                    console.log('Checksum:  ', data.checksum);
-                }
-            });
-        });
-    }
-});
+        }
+    });
+};
